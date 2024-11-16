b76a0292d30983772412953a0dd8e50d0a0e2643
pannavich <pannavich1911@gmail.com>
2024-11-16 22:32:24 +0700

    feat: add flow to see token flex first

--- src/modules/telegram/telegram.module.ts
+++ src/modules/telegram/telegram.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { TelegramUpdate } from './telegram.update';
 import { FirebaseModule } from '../firebase/firebase.module';
 import { EncryptionModule } from '../encryption/encryption.module';
+import { OneInchModule } from '../1inch/oneinch.module';
 
 @Module({
-  imports: [FirebaseModule, EncryptionModule],
+  imports: [FirebaseModule, EncryptionModule, OneInchModule],
   providers: [TelegramUpdate],
 })
 export class TelegramModule {}
