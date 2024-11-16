22a5441f7130da0e931bc08e5e466cb5d12745d8
apemon <760108+apemon@users.noreply.github.com>
2024-11-17 03:10:22 +0700

    fix bug build error

--- src/modules/position/position.module.ts
+++ src/modules/position/position.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { OneInchModule } from '../1inch/oneinch.module';
 import { PositionService } from './position.service';
+import { FirebaseModule } from '../firebase/firebase.module';
 
 @Module({
-  imports: [OneInchModule],
+  imports: [OneInchModule, FirebaseModule],
   providers: [PositionService],
   exports: [PositionService],
 })
